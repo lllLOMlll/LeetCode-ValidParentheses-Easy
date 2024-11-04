e83cab7a596da7bbf89d952f8ebbeda6b1523b59
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-03 21:50:46 -0500

    Parentheses not working if (()). Must find a solution for 2 ((

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ class Program
         public bool IsValid(string s) {
         // How long is the string
         int stringLength = s.Length;
+        // General Flag
+        bool isStringValid = true;
         // Flags for Parentheses and Brackets
         bool isParenthesesOpen = false;
         bool isBracketsOpen = false;
@@ -14,24 +16,24 @@ class Program
         {
             if (charOfs == '(' && isParenthesesOpen == false)
             {
-                isParenthesesOpen = true;
+                isParenthesesOpen = true;             
             } 
-            
-            if (charOfs == '(' && isParenthesesOpen == true)
+            else if (charOfs == '(' && isParenthesesOpen == true)
             {
-                return false;
+                return false;              
             }
 
             if (charOfs == ')' && isParenthesesOpen == true)
             {
-                isParenthesesOpen = false;
-            }
-
-            if (charOfs == ')' && isParenthesesOpen == false)
+                isParenthesesOpen = false;             
+            } 
+            else if (charOfs == ')' && isParenthesesOpen == false)
             {
                 return false;
             }
         }
+
+        
         return true;
     }
 
@@ -39,8 +41,20 @@ static void Main(string[] args)
 {
     Program p = new Program();
     // Test 1
-    string test1 = "()";
-    Console.WriteLine("() = " + p.IsValid(test1));
+    // string test1 = "()";
+    // Console.WriteLine("() = " + p.IsValid(test1));
+
+    // Test 1.1
+    string test1_1 = "(())";
+    Console.WriteLine("(()) = " + p.IsValid(test1_1));
+
+    // // Test 1.2
+    // string test1_2 = "(()";
+    // Console.WriteLine("(() = " + p.IsValid(test1_2));
+
+    // // Test 1.3
+    // string test1_3 = "(())(";
+    // Console.WriteLine("(())( = " + p.IsValid(test1_3));
 
     // // Test 2
     // string test2 = "()[]{}";
