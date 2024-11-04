f6dfa658cab28c0b33b2e533fbe7b2f31bc26de2
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-03 21:19:12 -0500

    Firs commit. Tests writtent

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program 
+{
+        public bool IsValid(string s) {
+        int stringLength = s.Length;
+        bool isParentheseOpen = false;
+
+        return true;
+    }
+
+static void Main(string[] args)
+{
+    Program p = new Program();
+    // Test 1
+    string test1 = "()";
+    Console.WriteLine("() = " + p.IsValid(test1));
+
+    // // Test 2
+    // string test2 = "()[]{}";
+    // Console.WriteLine("()[]{} = " + p.IsValid(test2));
+
+    // // Test 3
+    // string test3 = "(]";
+    // Console.WriteLine("(] = " + p.IsValid(test3));
+
+    // // Test 4
+    // string test4 = "(]";
+    // Console.WriteLine("(] = " + p.IsValid(test4));
+}
+}
